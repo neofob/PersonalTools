ed7d5e93137e75aa31e8cc48e089178eb875b429
Alexander Suvorov <asuvorov-softheme@users.noreply.github.com>
2013-10-14 16:29:17 +0300

    Update countBin.pl
    Someone else wrote a good tool 'utiq' that has an option '-c', so this script is another one bycicle :)

--- Programming/countBin.pl
+++ Programming/countBin.pl
@@ -1,9 +1,8 @@
 #!/usr/bin/perl
-# author: Tuan T. Pham
-# I wrote this tool to do some histogram of my weblog or any debug log
+# Someone else wrote a good tool 'utiq' that has an option '-c', so this script 
+# is another one bycicle :)
 # Example: to find out the frequency of different requests from bingbot
-# $ grep bingbot weblog.log | awk '{print $7}' | sort > bingbot.txt
-# countBin.pl bingbot.txt > bingCount.txt
+# $ grep bingbot weblog.log | awk '{print $7}' | sort | uniq -c > bingCount.txt
 # The output of file "bingCount.txt" will be something like this
 # /CDE/Spring06/CDE-Spring06-09.html	1
 # /excursions/Autumn04/Autumn04-09-BW.html	1
@@ -11,31 +10,3 @@
 # /fav/Cascades-2002-02.html	2
 # /robots.txt	46
 # Then I can plot them nicely in OpenOffice Calc
-
-$inputFile=@ARGV[0];
-
-open (INPUT_FILE,"$inputFile")
-|| die "Failed to open $inputFile\n";
-
-$lastFreq=1;
-$lastBin=<INPUT_FILE>;
-chomp $lastBin;
-
-while ($line=<INPUT_FILE>)
-{
-  chomp $line;
-  if ($line eq $lastBin)
-  {
-    $lastFreq++;
-  }
-  else
-  {
-    print "$lastBin\t$lastFreq\n";
-    $lastFreq=1;
-    $lastBin=$line;
-  }
-}
-
-print "$lastBin\t$lastFreq\n";
-
-close (INPUT_FILE);
